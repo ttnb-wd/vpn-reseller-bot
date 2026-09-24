e2c8bfbc0c2df00946770acc73cae6ad3b2ec482
ttnb-wd <maybelasttime9@gmail.com>
2026-09-24 22:14:21 +0630

    add express server

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('VPN Bot is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
 require("dotenv").config();
 
 const crypto = require("crypto");
